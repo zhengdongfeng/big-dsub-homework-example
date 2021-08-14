879fd9e6e46c165ebcc06f042494957dbfb52df7
zhengdongfeng <58932073+zhengdongfeng@users.noreply.github.com>
2021-08-14 10:01:18 +0800

    modify dsub.sh

--- dsub-php.sh
+++ dsub-php.sh
@@ -3,5 +3,8 @@
 #PBS -q core40 
 #PBS -l mem=1gb,nodes=1:ppn=3,walltime=2400:10:00
 #HSCHED -s no_project_name+no_software_name+no_species_name
-php /xtdisk/xueyb_group/zhengdf/dsub_submit_homework/myPhp.php > /xtdisk/xueyb_group/zhengdf/dsub_submit_homework/phpresult
-echo hello > /xtdisk/xueyb_group/zhengdf/dsub_submit_homework/hello
+
+cd /xtdisk/xueyb_group/zhengdf/dsub_submit_homework/ #先cd到工作目录,之后用相对路径，否则用绝对路径，不然结果及日志输出到家目录
+
+php ./myPhp.php > ./phpresult
+echo hello > ./hello
